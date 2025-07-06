d2dc30a0e8c8cf8895cb847561e91f054ec8b822
Kevin-advop <kevin.l@advop.co.in>
2025-07-07 00:08:39 +0530

    Login Button Added

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/components/LoginButton.module.css";
+
+const LoginButton = () => {
+  return (
+    <div>
+      <button className={styles.loginButton}>Login / Sign Up</button>
+    </div>
+  )
+}
+
+export default LoginButton
