0d9e3b3f7a76aa45ed77481ed3313286282dec2a
abhi-ananthu <162581534+abhi-ananthu@users.noreply.github.com>
2025-07-22 16:22:33 +0530

    added gloable prisma client

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,29 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
-const router =  Router();
+const router = Router();
 
-router.post("/create-user", (req, res) => {
-    res.send("User created");
+router.post('/create-user', (req, res) => {
+  res.send('User created');
 });
 
-router.get("/view-user/:id", (req, res) => {
-    res.send(`User details for ID: ${req.params.id}`);
+router.get('/view-user/:id', (req, res) => {
+  res.send(`User details for ID: ${req.params.id}`);
 });
 
-router.post("/update-user/:id", (req, res) => {
-    res.send(`User details updated for ID: ${req.params.id}`);
+router.post('/update-user/:id', (req, res) => {
+  res.send(`User details updated for ID: ${req.params.id}`);
 });
 
-router.get("/delete-user/:id", (req, res) => {
-    res.send(`User deleted with ID: ${req.params.id}`);
+router.get('/delete-user/:id', (req, res) => {
+  res.send(`User deleted with ID: ${req.params.id}`);
 });
 
-router.get("/block-user/:id", (req, res) => {
-    res.send(`User blocked with ID: ${req.params.id}`);
+router.get('/block-user/:id', (req, res) => {
+  res.send(`User blocked with ID: ${req.params.id}`);
 });
 
-router.get("/unblock-user/:id", (req, res) => {
-    res.send(`User unblocked with ID: ${req.params.id}`);
+router.get('/unblock-user/:id', (req, res) => {
+  res.send(`User unblocked with ID: ${req.params.id}`);
 });
+
+router.get('/get-matched-user/:id', (req, res) => {});
