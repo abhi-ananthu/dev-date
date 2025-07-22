d7d2ab6c7dfcb28dc19816b9a586fca64edb4d7e
abhi-ananthu <162581534+abhi-ananthu@users.noreply.github.com>
2025-07-22 14:43:41 +0530

    change: matching, del accept and reject

--- controller/matching/middleware.js
+++ controller/matching/middleware.js
@@ -1,5 +1,1 @@
 export const requestUserMiddleware = async (req, res, next) => {};
-
-export const acceptRequestMiddleware = async (req, res, next) => {};
-
-export const rejectRequestMiddleware = async (req, res, next) => {};
--- server.js
+++ server.js
@@ -2,17 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import onboardingRouter from './routes/onboardingRoute';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send({ message: 'welcome to dev date api' });
+io.on('connection', (socket) => {
+  socket.broadcast('user connected');
 });
 
-app.use('/', onboardingRouter);
-
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(
     `server listening on ${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}/`
   );
