155dca8975c70c19edcaa33ae03f749aefb5fbcf
abhi-ananthu <162581534+abhi-ananthu@users.noreply.github.com>
2025-07-22 16:10:13 +0530

    added reference for swiper and target

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ dotenv.config();
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 const server = createServer(app);
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-  socket.broadcast('user connected');
+  socket.on('message', (msg) => {
+    socket.send(msg);
+  });
 });
 
 server.listen(process.env.PORT, () => {
