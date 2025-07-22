811b957cd1407f82551ba27305bf4a22d1ce9433
Abhiram-advop <217032832+Abhiram-advop@users.noreply.github.com>
2025-07-22 11:58:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'welcome to dev date api' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `server listening on ${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}/`
+  );
+});
